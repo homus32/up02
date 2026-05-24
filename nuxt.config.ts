394dd56224d1182c897e2d2f5f4e380c4fd50d69
homus32 <homus32.reset@mail.ru>
2026-05-24 15:07:41 +1100

    chore: add devServer host config for local network access

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,6 +59,7 @@ const AnimeBazaPreset = definePreset(Aura, {
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  devServer: { host: '0.0.0.0' },
 
   components: [
     { path: '~/components', pathPrefix: false },
